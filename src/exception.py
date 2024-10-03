d148862afe64b67ccde70a8faf60a6e6526468d9
Ranjith7935 <rkranjithkumar009@gmail.com>
2024-10-04 00:04:09 +0530

    Looging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -14,5 +14,10 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
-
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by 0")
+        raise CustomException(e,sys)
     
